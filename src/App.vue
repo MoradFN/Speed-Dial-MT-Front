2cabf5d525c0cef2f61295d79b05677ce850c84d
MoradFN <71790104+MoradFN@users.noreply.github.com>
2024-11-13 09:43:19 +0100

    working

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import Navbar from "@/components/Navbar.vue";
 import logContact from "@/components/logContact.vue";
 import { RouterView } from "vue-router";
 </script>
-
+<!-- working -->
 <template>
   <Navbar />
   <logContact />
