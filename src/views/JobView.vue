25ba3ca06e84d2b4ad3d242b1fc199b6d9a706cb
MoradFN <71790104+MoradFN@users.noreply.github.com>
2024-11-15 14:19:31 +0100

    Speed Dialer, just presentation.

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -141,18 +141,18 @@ onMounted(async () => {
         <aside>
           <!-- Manage Target List -->
           <div class="bg-white p-6 rounded-lg shadow-md mt-6">
-            <h3 class="text-xl font-bold mb-6">Manage Target List</h3>
+            <h3 class="text-xl font-bold mb-6">Speed Diealer</h3>
             <RouterLink
               :to="`/target-lists/edit/${state.targetList.id}`"
               class="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
             >
-              Edit Target List
+              Start Speed Dialer
             </RouterLink>
             <button
               @click="deleteTargetList"
-              class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+              class="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
             >
-              Delete Target List
+              Check Recent Logs
             </button>
           </div>
         </aside>
